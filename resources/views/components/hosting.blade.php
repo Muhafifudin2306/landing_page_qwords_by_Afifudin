492047d4568e9a87835e3215531019470c827b00
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-12-06 05:47:56 +0700

    hosting section

---
+++ resources/views/components/hosting.blade.php
@@ -0,0 +1,84 @@
+<!-- ============================================-->
+<!-- Section Domain ============================-->
+<!-- ============================================-->
+
+<section id="hosting">
+      <div class="container">
+        <div class="title-hosting">
+          <h2 class="text-center display-5 fw-semi-bold" data-zanim-xs='{"delay":1.2}' data-zanim-trigger="scroll"> Cloud Hosting dan VPS Diskon Hingga 60% !</h2>
+          <p class="mt-3" style="font-size: 18px" align="center" data-zanim-xs='{"delay":1.6}' data-zanim-trigger="scroll"> Layanan unggulan dengan teknologi terbaik untuk website anda</p>
+        </div>
+        <div class="content_hosting" style="margin-top: 80px" data-zanim-xs='{"delay":2.0}' data-zanim-trigger="scroll">
+          <div class="row">
+            <div class="col-lg-3">
+              <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="assets/img/icons/1.png" alt="Dashboard" style="width:95px;" />
+                <h4 class="mt-3 lh-base" style="color: rgb(255, 140, 0)">Value Hybrid Performance</h4>
+                <h5 class="mt-2" style="font-size:18px;color:rgb(249, 67, 67)"> <strike>Rp 24.500</strike> </h5>
+                <h5 class="mt-2" style="font-size:24px"> Rp 14.500 </h5>
+                <button class="btn btn-sm btn-primary login">Order Sekarang</button>
+                <ul class="list-unstyled py-3 mt-3" align="left">
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>3 GB Disk
+                  </li>
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Bandwidth 
+                  </li>
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>0.5 Core CPU
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="assets/img/icons/1.png" alt="Dashboard" style="width:95px;" />
+                <h4 class="mt-3 lh-base" style="color:rgb(255, 140, 0)">Unlimited Hosting</h4>
+                <h5 class="mt-2" style="font-size:18px;color:rgb(249, 67, 67)"> <strike>Rp 109.000</strike> </h5>
+                <h5 class="mt-2" style="font-size:24px"> Rp 84.900 </h5>
+                <button class="btn btn-sm btn-primary login">Order Sekarang</button>
+                <ul class="list-unstyled py-3 mt-3" align="left">
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Disk
+                  </li>
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Bandwidth 
+                  </li>
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>1 Core CPU
+                  </li>
+                  <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>5/5 Addon domain
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3">
+                  <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="assets/img/icons/1.png" alt="Dashboard" style="width:95px;" />
+                        <h4 class="mt-3 lh-base" style="color:rgb(255, 140, 0)">High Performance</h4>
+                        <h5 class="mt-2" style="font-size:18px;color:rgb(249, 67, 67)"> <strike>Rp 80.000</strike> </h5>
+                        <h5 class="mt-2" style="font-size:24px"> Rp 69.000 </h5>
+                        <button class="btn btn-sm btn-primary login">Order Sekarang</button>
+                        <ul class="list-unstyled py-3 mt-3" align="left">
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Disk
+                          </li>
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Bandwidth 
+                          </li>
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>1 Core CPU
+                          </li>
+                        </ul>
+                      </div>
+            </div>
+            <div class="col-lg-3">
+                  <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="assets/img/icons/1.png" alt="Dashboard" style="width:95px;" />
+                        <h4 class="mt-3 lh-base" style="color:rgb(255, 140, 0)">Cloud VPS KVM SSD</h4>
+                        <h5 class="mt-2" style="font-size:18px;color:rgb(249, 67, 67)"> <strike>Rp 135.000</strike> </h5>
+                        <h5 class="mt-2" style="font-size:24px"> Rp 75.000 </h5>
+                        <button class="btn btn-sm btn-primary login">Order Sekarang</button>
+                        <ul class="list-unstyled py-3 mt-3" align="left">
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>25 GB Disk
+                          </li>
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Bandwidth 
+                          </li>
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>1 Core CPU
+                          </li>
+                          <li class="mb-3 text-900"><span class="far fa-check-circle text-700 me-2"></span>Unlimited Addon domain
+                          </li>
+                        </ul>
+                      </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
