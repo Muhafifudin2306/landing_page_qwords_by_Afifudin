6d8f8ac1cceb650ad892a7d4faaa61e42b4bddbc
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-12-06 00:09:22 +0700

    Domain section

---
+++ resources/views/components/domain.blade.php
@@ -0,0 +1,13 @@
+     <!-- ============================================-->
+     <!-- Section Domain ============================-->
+     <!-- ============================================-->
+     <section id="customers">
+      <div class="container col-6">
+        <h1 class="text-center display-5 fw-semi-bold" data-zanim-xs='{"delay":0.3}' data-zanim-trigger="scroll"> Cari Nama Domain</h1>
+        <p class="mt-3" style="font-size: 18px" align="center" data-zanim-xs='{"delay":0.7}' data-zanim-trigger="scroll"> Masukkan nama website Anda dan pilih ekstensi domain <br class="d-none d-lg-block" />(tersedia ekstensi .com, .id, .online, .live, .store, .info, dll).</p>
+        <div class="input-group mt-4" data-zanim-xs='{"delay":1.1}' data-zanim-trigger="scroll">
+          <input class="form-control" type="text" placeholder="Ketik Nama Domain" />
+          <button class="btn btn-primary rounded-start-0 px-3 login" type="submit">Cek Domain</button>
+        </div>
+      </div>
+    </section>
