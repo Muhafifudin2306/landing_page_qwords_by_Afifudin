1f074d52338bf2dc932cbedf02c9264049ed4159
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-12-06 09:42:49 +0700

    Commitmen Section

---
+++ resources/views/components/commitment.blade.php
@@ -0,0 +1,50 @@
+<!-- ============================================-->
+<!-- Section Commitnment ========================-->
+<!-- ============================================-->
+
+<section id="commitment">
+      <div class="container">
+        <div class="title-commitment">
+          <h2 class="text-center display-5 fw-semi-bold" data-zanim-xs='{"delay":0.5}' data-zanim-trigger="scroll">  Komitmen Qwords</h2>
+          <p class="mt-3" style="font-size: 18px" align="center" data-zanim-xs='{"delay":0.6}' data-zanim-trigger="scroll"> Layanan unggulan dengan teknologi terbaik untuk website anda</p>
+        </div>
+        <div class="commitment-content" data-zanim-xs='{"delay":0.8}' data-zanim-trigger="scroll">
+          <div class="row mb-4 mt-8">
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/10.png') }}" alt="Dashboard" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Layanan 24/7</h4>
+              <p class="fs-0 mt-3" align="justify">Tim Technical Support dan Customer Service Qwords siap membantu Anda selama 24 jam, 7 hari. Anda dapat menghubungi melalui Live chat, Call Center, dan Support Ticket.</p>
+            </div>
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/9.png') }}" alt="Comment" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Panduan Manual Lengkap</h4>
+              <p class="fs-0 mt-3" align="justify">Qwords dilengkapi dengan halaman knowledge base yang berisi tutorial dan tips seputar pengelolaan website. Dapat diakses dengan mudah dan solutif.</p>
+            </div>
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/11.png') }}" alt="Tailored" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Uptime 99.99%</h4>
+              <p class="fs-0 mt-3" align="justify">Uptime Network & Server di atas 99.99% dengan konfigurasi server yang tepat. Didukung manajemen jaringan dan multiple upstream tier 1 provider network serta multiple peering.</p>
+            </div>
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/6.png') }}" alt="Dashboard" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Jaminan keamanan</h4>
+              <p class="fs-0 mt-3" align="justify">Keamanan menjadi fokus utama kami di Qwords. Tindakan pemantauan dan pencegahan yang berstandar & berkelanjutan telah menjadi perhatian utama kami.</p>
+            </div>
+          </div>
+          <div class="row mb-6 mt-5">
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/13.png') }}" alt="Dashboard" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Clustered DNS</h4>
+              <p class="fs-0 mt-3" align="justify">DNS Server Cloud Hosting Qwords terhubung satu dengan yang lainnya. Hal ini membuat DNS Server akan selalu dapat di akses karena saling membackup antar server.</p>
+            </div>
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/4.png') }}" alt="Dashboard" style="width:95px;" />
+              <h4 class="mt-3 lh-base">High Enterprise Server</h4>
+              <p class="fs-0 mt-3" align="justify">Standar server Qwords adalah server kelas enterprise Dual Xeon Octa Core yang merupakan hardware kelas terbaik. Hardware ini biasa digunakan oleh perusahaan terkemuka.</p>
+            </div>
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/15.png') }}" alt="Dashboard" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Tier 1 Network</h4>
+              <p class="fs-0 mt-3" align="justify">Tier 1 Network merupakan jaringan dunia tanpa transit network, yang membuat jaringan lebih cepat serta jalur routing yang lebih pendek dan ketersediaan backup network.</p>
+            </div>
+            <div class="col-md-6 col-lg-3 text-center"><img src="{{ asset('assets/img/icons/14.png') }}" alt="Dashboard" style="width:95px;" />
+              <h4 class="mt-3 lh-base">Peering Network</h4>
+              <p class="fs-0 mt-3" align="justify">Jaringan di Qwords terhubung langsung ke public peering seperti OpenIXP dan CDIX yang membuat latency atau waktu tunggu pengiriman data semakin kecil.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
--- resources/views/components/hosting.blade.php
+++ resources/views/components/hosting.blade.php
@@ -5,10 +5,10 @@
 <section id="hosting">
       <div class="container">
         <div class="title-hosting">
-          <h2 class="text-center display-5 fw-semi-bold" data-zanim-xs='{"delay":1.2}' data-zanim-trigger="scroll"> Cloud Hosting dan VPS Diskon Hingga 60% !</h2>
-          <p class="mt-3" style="font-size: 18px" align="center" data-zanim-xs='{"delay":1.6}' data-zanim-trigger="scroll"> Layanan unggulan dengan teknologi terbaik untuk website anda</p>
+          <h2 class="text-center display-5 fw-semi-bold" data-zanim-xs='{"delay":0.5}' data-zanim-trigger="scroll"> Cloud Hosting dan VPS Diskon Hingga 60% !</h2>
+          <p class="mt-3" style="font-size: 18px" align="center" data-zanim-xs='{"delay":0.6}' data-zanim-trigger="scroll"> Layanan unggulan dengan teknologi terbaik untuk website anda</p>
         </div>
-        <div class="content_hosting" style="margin-top: 80px" data-zanim-xs='{"delay":2.0}' data-zanim-trigger="scroll">
+        <div class="content_hosting" style="margin-top: 80px" data-zanim-xs='{"delay":0.8}' data-zanim-trigger="scroll">
           <div class="row">
             <div class="col-lg-3">
               <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="assets/img/icons/1.png" alt="Dashboard" style="width:95px;" />
