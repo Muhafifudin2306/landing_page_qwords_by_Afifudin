6b1a031cbf81a897d6f8025e4651b68f09fb3b90
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-12-06 10:25:32 +0700

    Footer Section

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,96 @@
+      <!-- ============================================-->
+      <!-- Section Footer =============================-->
+      <!-- ============================================-->
+      <section class="py-0 bg-1000">
+            <div class="container">
+              <div class="row py-7">
+                <div class="col-12 col-sm-12 col-lg-3 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="#"><img src="{{ asset('assets/img/icons/Logo_footer.png') }}" height="51" alt="" /></a>
+                  <p class="text-light mt-4 mb-0">tel : +628 041 808 888</p>
+                  <p class="text-light mb-0">tel : 021 397 088 00</p>
+                  <p class="text-light mb-0">WA : +6281 7437 111</p>
+                  <p class="text-light mt-5">info@qwords.com</p>
+                  <div class="d-flex">
+                        <a href="https://www.youtube.com/user/qwordsvideo">
+                              <svg class="me-xl-3 me-1" xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="white" class="bi bi-youtube" viewBox="0 0 16 16">
+                              <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                      </svg>
+                  </a>
+                  <a href="https://www.facebook.com/qwordsdotcom">
+                        <img class="me-xl-3 me-1" src="{{ asset('assets/img/gallery/facebook-line1.svg') }}" alt="fb" width:'20px'="width:'20px'" height:'20px'="height:'20px'" />
+                  </a>
+                  <a href="https://twitter.com/qwordsdotcom">
+                        <img class="me-xl-3 me-1" src="{{ asset('assets/img/gallery/twitter-line1.svg') }}" alt="twitter" width:'20px'="width:'20px'" height:'20px'="height:'20px'" />
+                  </a>
+                  <a href="https://www.instagram.com/qwordsdotcom/">
+                        <img class="me-xl-3 me-1" src="{{ asset('assets/img/gallery/instagram-line1.svg') }}" alt="instragram" width:'20px'="width:'20px'" height:'20px'="height:'20px'" />
+                  </a>
+                  </div>
+                </div>
+                <div class="col-3 col-sm-4 col-lg-3 mb-3 order-2 order-sm-1">
+                  <h5 class="lh-lg fw-bold text-light font-sans-serif">Mengenai Kami</h5>
+                  <ul class="list-unstyled text-light mb-md-4">
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Corporate Website</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Qwords History</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Qwords Differences</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Media Publication</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Network & Infrastructure</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Partner</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Media Kit</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Career & Employment</a></li>
+                  </ul>
+                </div>
+                <div class="col-6 col-sm-4 col-lg-3 mb-3 order-3 order-sm-2">
+                  <h5 class="lh-lg fw-bold text-light font-sans-serif">MSA/SLA/AUP</h5>
+                  <ul class="list-unstyled text-light mb-md-4 mb-lg-0">
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Master Service Agreement</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Refund Policy</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Privacy Policy</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Product Life Cycle Policy</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Penggunaan Merek dan Logo</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Faktur Pajak</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Migrasi Ke Qwords</a></li>
+                  </ul>
+                </div>
+                <div class="col-6 col-sm-4 col-lg-3 mb-3 order-3 order-sm-2">
+                  <h5 class="lh-lg fw-bold text-light font-sans-serif"> Layanan Reseller dan Lainnya</h5>
+                  <ul class="list-unstyled text-light mb-md-4 mb-lg-0">
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Afiliasi</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Qwords Partner</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Private Label cPanel Reseller</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Private Label Domain Reseller</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Standard Support</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Flexi Main Domain</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">FTP Backup</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Hosting Insurance</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+    
+    
+            <!-- ============================================-->
+            <!-- <section> begin ============================-->
+            <section class="py-0 bg-1000">
+    
+              <div class="container">
+                <div class="row justify-content-md-between justify-content-evenly py-3">
+                  <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
+                    <p class="fs--1 my-2 fw-bold text-200">&copy; PT Qwords Company International</p>
+                  </div>
+                  <div class="col-12 col-sm-8 col-md-6">
+                    <p class="fs--1 my-2 text-center text-md-end text-200"> Made with&nbsp;
+                      <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#F95C19" viewBox="0 0 16 16">
+                        <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path>
+                      </svg>&nbsp;by&nbsp;<a class="fw-bold text-info text-200" href="https://afifportofolio.000webhostapp.com/" target="_blank">Afifudin </a> and <a class="fw-bold text-info text-200" href="https://themewagon.com/" target="_blank">ThemeWagon </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <!-- end of .container-->
+    
+            </section>
+            <!-- <section> close ============================-->
+            <!-- ============================================-->
+    
+    
+          </section>
