870e274a2748361ca513bbd009c736c910b99938
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-12-06 11:30:36 +0700

    Product and Web Section

---
+++ resources/views/components/product.blade.php
@@ -0,0 +1,52 @@
+<!-- ============================================-->
+<!-- Section Product ============================-->
+<!-- ============================================-->
+
+<section id="hosting">
+      <div class="container">
+        <div class="title-hosting">
+          <h2 class="text-center display-5 fw-semi-bold" data-zanim-xs='{"delay":0.5}' data-zanim-trigger="scroll"> Lihat Juga Layanan Kami Yang Lain</h2>
+          <p class="mt-3" style="font-size: 18px" align="center" data-zanim-xs='{"delay":0.6}' data-zanim-trigger="scroll"> Layanan unggulan dengan teknologi terbaik untuk website anda</p>
+        </div>
+        <div class="content_hosting" style="margin-top: 80px" data-zanim-xs='{"delay":0.8}' data-zanim-trigger="scroll">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="{{ asset('assets/img/icons/flex_server.png') }}" style="width:95px;" />
+                <h3 class="mt-4 lh-base" style="color: rgb(255, 140, 0)">SSL</h3>
+                <ul class="list-unstyled py-3 mt-2" align="left">
+                      <li align="justify" class="mb-4 text-900">Jadikan website Anda lebih terpercaya dan disukai oleh search engine dengan menggunakan SSL Certificate.
+                  </li>
+            </ul> <br>
+            <h5 class="mt-2" style="font-size:18px;font-weight:400"> Mulai dari </h5>
+            <h5 class="mt-2" style="font-size:24px"> Rp 112.000/thn </h5>
+            <button class="btn btn-md btn-primary login">Order Sekarang</button>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="{{ asset('assets/img/icons/flex_server.png') }}" style="width:95px;" />
+                <h3 class="mt-4 lh-base" style="color:rgb(255, 140, 0)">Dedicated Server</h3>
+                <ul class="list-unstyled py-3 mt-2" align="left">
+                      <li align="justify" class="mb-4 text-900">Nikmati kelola Dedicated Server sendiri dengan akses remote dari seluruh dunia tanpa batas
+                  </li>
+            </ul><br><br>
+            <h5 class="mt-2" style="font-size:18px;font-weight:400"> Mulai dari </h5>
+            <h5 class="mt-2" style="font-size:24px"> Rp 499.000/bln </h5>
+            <button class="btn btn-md btn-primary login">Order Sekarang</button>
+              </div>
+            </div>
+            <div class="col-lg-4">
+                  <div class="text-center w-100 h-100 p-5 services-card-shadow rounded-4"><img src="{{ asset('assets/img/icons/flex_server.png') }}" style="width:95px;" />
+                        <h3 class="mt-4 lh-base" style="color:rgb(255, 140, 0)">Colocation Server</h3>
+                        <ul class="list-unstyled py-3 mt-2" align="left">
+                              <li align="justify" class="mb-2 text-900">Akses cepat dari seluruh dunia dengan fasilitas 24x7 full monitoring dengan cooling system terbaik serta fire extinguisher
+                              </li>
+                        </ul><br>
+                        <h5 class="mt-2" style="font-size:18px;font-weight:400"> Mulai dari </h5>
+                        <h5 class="mt-2" style="font-size:24px"> Rp 500.000/bln </h5>
+                        <button class="btn btn-md btn-primary login">Order Sekarang</button>
+                      </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
---
+++ resources/views/components/web_build.blade.php
@@ -0,0 +1,14 @@
+     <!-- ============================================-->
+     <!-- Section Web Build ==========================-->
+     <!-- ============================================-->
+
+<div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 mb-4"><img class="w-100" src="{{ asset('assets/img/illustration/5.png') }}" alt="..." /></div>
+        <div class="col-md-6 ml-8 text-center text-md-start">
+          <h4 class="fs-3 fw-normal" data-zanim-xs='{"delay":0.3}' data-zanim-trigger="scroll">Bikin website impian dengan mudah disini, Hanya 1 Jutaan!</h4>
+          <p class="fs-0 pe-xl-7" data-zanim-xs='{"delay":0.5}' data-zanim-trigger="scroll">Jasa pembuatan website profesional untuk Perusahaan, Bisnis & UKM. Bikin Website Impian dengan mudah disini, desain website elegan dan mudah dikelola.</p>
+          <button class="btn btn-sm btn-primary login my-2 me-1" data-zanim-xs='{"delay":0.7}' data-zanim-trigger="scroll" href="#!" role="button">Lihat Selengkapnya</button>
+        </div>
+      </div>
+    </div>
